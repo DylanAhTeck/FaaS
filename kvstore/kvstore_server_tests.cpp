8b27e092b7ead2c7243d696a28ec8b5dc04c559e
Dylan <ahteck@usc.edu>
2020-02-03 23:12:09 -0800

    Set up kvstore_server_test.cpp to be ready for tests.
    Q: I am following https://github.com/grpc/grpc/blob/master/test/cpp/end2end/mock_test.cc for reference.
    Should we also create FakeClients and follow this example to test our client and server?
    Should we also mirror their type of tests, for example tests BidiStream, SimpleRpc, etc in their MockTest for testing the server?

--- kvstore/kvstore_server_tests.cpp
+++ kvstore/kvstore_server_tests.cpp
@@ -5,6 +5,9 @@
 
 #include "../grpc/test/core/util/port.h"
 
+#include "client.h"
+#include "kvstore_server.h"
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -40,18 +43,38 @@ protected:
     {
         // Code here will be called immediately after the constructor (right
         // before each test).
+
+        int port = grpc_pick_unused_port_or_die();
+        server_address_ << "localhost:" << port;
+        // Setup server
+        ServerBuilder builder;
+        builder.AddListeningPort(server_address_.str(),
+                                 grpc::InsecureServerCredentials());
+        builder.RegisterService(&service_);
+        server_ = builder.BuildAndStart();
     }
 
     // Code here will be called immediately after each test (right
     // before the destructor).
     void TearDown() override {}
 
+    void ResetStub()
+    {
+        std::shared_ptr<Channel> channel = grpc::CreateChannel(
+            server_address_.str(), grpc::InsecureChannelCredentials());
+        stub_ = KeyValueStore::NewStub(channel);
+    }
+
     // Class members declared here can be used by all tests in the test suite
     // for Foo.
+    std::unique_ptr<KeyValueStore::Stub> stub_;
+    std::unique_ptr<Server> server_;
+    std::ostringstream server_address_;
+    KeyValueStoreServiceImpl service_;
 };
 
-// Tests that the Foo::Bar() method does Abc.
-TEST_F(KvstoreServerTests, TestPut)
+// Test a real RPC and mocked RPC for PUT and REMOVE requests
+TEST_F(KvstoreServerTests, PUT)
 {
 }
 
